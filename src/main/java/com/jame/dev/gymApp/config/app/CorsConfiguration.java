ff70447bc7677069363335063e93342d4cb5ba90
jame-dev13 <jam.dev013@gmail.com>
2025-11-26 21:02:15 -0600

    feat(config): created config classes focus on the web and security configuration.

---
+++ src/main/java/com/jame/dev/gymApp/config/app/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.jame.dev.gymApp.config.app;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Component
+public class CorsConfiguration {
+
+   private final List<String> ALLOWED_ORIGINS = List.of("http://locahost:5173");
+   private final List<String> ALLOWED_METHODS = List.of(
+           "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+   private final List<String> ALLOWED_HEADERS = List.of(
+           "Content-Type", "Authorization", "Access-Control-Allow-Headers");
+
+   public void configurationSource(){
+      org.springframework.web.cors.CorsConfiguration cors = new org.springframework.web.cors.CorsConfiguration();
+      cors.setAllowedOrigins(ALLOWED_ORIGINS);
+      cors.setAllowedMethods(ALLOWED_METHODS);
+      cors.setAllowedHeaders(ALLOWED_HEADERS);
+      cors.setAllowCredentials(true);
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", cors);
+   }
+}
