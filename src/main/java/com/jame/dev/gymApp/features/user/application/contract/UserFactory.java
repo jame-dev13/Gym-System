d977abe699e6f8f2d015b3930cf6fb490220cac2
jame-dev13 <jam.dev013@gmail.com>
2026-05-11 20:58:02 -0600

    fix(project): fixed some semantic troubles, Factory interface was sealed but packages got separated, sealed removed. SubscriptionMetricsRepository and EarningMetricsRepository was having a typo in the package constructor declaration into the Query.

--- src/main/java/com/jame/dev/gymApp/features/user/application/contract/UserFactory.java
+++ src/main/java/com/jame/dev/gymApp/features/user/application/contract/UserFactory.java
@@ -8,7 +8,7 @@ import com.jame.dev.gymApp.features.user.api.response.UserResponse;
 import com.jame.dev.gymApp.features.user.api.response.UserMinimalInfoResponse;
 import org.springframework.data.domain.Page;
 
-public non-sealed interface UserFactory extends Factory<
+public interface UserFactory extends Factory<
    UserEntity, UserResponse, UserRequest> {
    PageDto<UserMinimalInfoResponse> createMinimalInfoPage(final Page<UserMinimalInfoResponse> page);
 }
