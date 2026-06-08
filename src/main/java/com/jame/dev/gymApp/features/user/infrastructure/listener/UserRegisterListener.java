6626c2f406ab58d392ca4740e1ad7a2118e6a3e7
jame-dev13 <jam.dev013@gmail.com>
2026-06-08 14:18:26 -0600

    fix: Fixed verifyAndNotify method workflow trouble that was trying to retrieve an unexisting VerificationEntity.

--- src/main/java/com/jame/dev/gymApp/features/user/infrastructure/listener/UserRegisterListener.java
+++ src/main/java/com/jame/dev/gymApp/features/user/infrastructure/listener/UserRegisterListener.java
@@ -7,7 +7,8 @@ import com.jame.dev.gymApp.features.auth.domain.model.VerificationEntity;
 import com.jame.dev.gymApp.features.notification.application.contract.EmailService;
 import com.jame.dev.gymApp.features.notification.application.dto.EmailDetails;
 import com.jame.dev.gymApp.features.notification.domain.model.HtmlTemplates;
-import com.jame.dev.gymApp.features.user.domain.model.UserEntity;
+import com.jame.dev.gymApp.features.user.application.contract.UserService;
+import com.jame.dev.gymApp.features.user.domain.exception.UserEntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -18,27 +19,33 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class UserRegisterListener {
+   private final UserService userService;
    private final EmailService emailService;
    private final VerificationService verificationService;
    private final TokenGeneratorService tokenGeneratorService;
 
    @EventListener
    @Async("taskExecutor")
-   public void verifyAndNotify(final UserNotifiableEvent user) {
-      if (!user.isNotifiable()) return;
-      final VerificationEntity verificationEntity = verificationService.getByUserEmail(user.input().email());
-      final UserEntity userEntity = verificationEntity.getUser();
-      final String rawToken = tokenGeneratorService.generateToken();
-      final VerificationEntity verificationSaved = verificationService.save(userEntity, rawToken);
-      verificationService.verify(verificationSaved, rawToken);
-      final String email = userEntity.getEmail();
-      final var emailDetails = new EmailDetails(
-         email,
-         HtmlTemplates.adminTemplate(
-            email,
-            user.input().password()),
-         "Welcome."
-      );
-      emailService.sendSimpleEmail(emailDetails);
+   public void verifyAndNotify(final UserNotifiableEvent event) {
+      final String email = event.email();
+      userService.getUserByEmail(email)
+         .ifPresentOrElse(
+            user -> {
+               final String rawToken = tokenGeneratorService.generateToken();
+               final VerificationEntity verificationSaved = verificationService.save(user, rawToken);
+               verificationService.verify(verificationSaved, rawToken);
+               final var emailDetails = new EmailDetails(
+                  email,
+                  HtmlTemplates.adminTemplate(
+                     email,
+                     event.rawPassword()),
+                  "Welcome."
+               );
+               emailService.sendSimpleEmail(emailDetails);
+
+            },
+            () -> {
+               throw new UserEntityNotFoundException("User not found.");
+            });
    }
 }
