ebeea4a90b8de36594796d606fdec25141761b7f
jame-dev13 <jam.dev013@gmail.com>
2026-07-30 12:58:37 -0600

    feat: Implemented new entity SubscriberNotificationEntity to set up the base to automatize notification process when a subscription is about to end, adjustable to user's wishes and comfortableness.

--- src/main/java/com/jame/dev/gymApp/features/notification/application/contract/SubscriberNotificationFactory.java
+++ src/main/java/com/jame/dev/gymApp/features/notification/application/contract/SubscriberNotificationFactory.java
@@ -1,13 +1,15 @@
 package com.jame.dev.gymApp.features.notification.application.contract;
 
-import com.jame.dev.gymApp.application.support.factories.Factory;
 import com.jame.dev.gymApp.features.notification.api.response.SubscriberNotificationResponse;
 import com.jame.dev.gymApp.features.notification.application.dto.SubscriberNotificationFactoryDtoInput;
 import com.jame.dev.gymApp.features.notification.domain.model.SubscriberNotificationEntity;
 import com.jame.dev.gymApp.features.subscription.domain.model.SubscriptionEntity;
 
-public interface SubscriberNotificationFactory extends Factory<
-   SubscriberNotificationEntity, SubscriberNotificationResponse, SubscriberNotificationFactoryDtoInput> {
+public interface SubscriberNotificationFactory {
+
+   SubscriberNotificationResponse createFromEntity(SubscriberNotificationEntity entity);
+
+   SubscriberNotificationEntity createFromInput(SubscriberNotificationFactoryDtoInput input);
 
    default SubscriberNotificationEntity from(SubscriptionEntity subscription, int rangeDays) {
       return SubscriberNotificationEntity.builder()
--- src/main/java/com/jame/dev/gymApp/features/notification/application/support/factory/SubscriberNotificationApplicationFactory.java
+++ src/main/java/com/jame/dev/gymApp/features/notification/application/support/factory/SubscriberNotificationApplicationFactory.java
@@ -1,26 +1,17 @@
 package com.jame.dev.gymApp.features.notification.application.support.factory;
 
-import com.jame.dev.gymApp.application.dto.PageDto;
-import com.jame.dev.gymApp.application.support.factories.PageDtoFactory;
 import com.jame.dev.gymApp.features.notification.api.response.SubscriberNotificationResponse;
 import com.jame.dev.gymApp.features.notification.application.contract.SubscriberNotificationFactory;
 import com.jame.dev.gymApp.features.notification.application.dto.SubscriberNotificationFactoryDtoInput;
 import com.jame.dev.gymApp.features.notification.application.support.mapper.SubscriberNotificationMapper;
 import com.jame.dev.gymApp.features.notification.domain.model.SubscriberNotificationEntity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SubscriberNotificationApplicationFactory implements SubscriberNotificationFactory {
    private final SubscriberNotificationMapper subscriberNotificationMapper;
-   private final PageDtoFactory<SubscriberNotificationEntity, SubscriberNotificationResponse> pageSubscriberNotificationFactory;
-
-   @Override
-   public PageDto<SubscriberNotificationResponse> createPageFrom(Page<SubscriberNotificationEntity> page) {
-      return pageSubscriberNotificationFactory.createPageDtoFrom(page);
-   }
 
    @Override
    public SubscriberNotificationResponse createFromEntity(SubscriberNotificationEntity entity) {
